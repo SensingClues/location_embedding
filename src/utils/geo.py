725798b3038feb615492645cbd31cfe0bef96c72
Richard Bartels <richard.bartels@vantage-ai.com>
2020-02-26 08:22:18 +0100

    Add osm tile api example

---
+++ src/utils/geo.py
@@ -0,0 +1,12 @@
+import math
+
+def deg2num(zoom, lat_deg, lon_deg):
+    lat_rad = math.radians(lat_deg)
+    n = 2.0 ** zoom
+    xtile = int((lon_deg + 180.0) / 360.0 * n)
+    ytile = int(
+        (1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi)
+        / 2.0
+        * n
+    )
+    return zoom, xtile, ytile
